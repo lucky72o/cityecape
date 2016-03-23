f8fef603a12860e6f203dec54254b06d6f2c0591
Slava <gurmanov.slava@gmail.com>
2016-03-23 20:30:11 +0000

    added rollback for repository tests, removed unneeded classes

--- src/test/java/com/cityescape/repository/AbstractCityEscapeRepositoryTest.java
+++ src/test/java/com/cityescape/repository/AbstractCityEscapeRepositoryTest.java
@@ -2,6 +2,7 @@ package com.cityescape.repository;
 
 import com.cityescape.Application;
 import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.test.annotation.Rollback;
 import org.springframework.test.context.junit4.AbstractTransactionalJUnit4SpringContextTests;
 import org.springframework.test.context.web.WebAppConfiguration;
 
@@ -10,5 +11,6 @@ import org.springframework.test.context.web.WebAppConfiguration;
  */
 @WebAppConfiguration //todo find out why this is required
 @SpringApplicationConfiguration(classes = {Application.class})
+@Rollback(value = false)
 public class AbstractCityEscapeRepositoryTest extends AbstractTransactionalJUnit4SpringContextTests {
 }
